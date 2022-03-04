8e614a21ed3841c8b5722b73baa8c17bc0618173
Badi-Haki Martin <jojackblack@gmail.com>
2022-03-03 22:00:52 -0500

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function superbowlWin(record){
+    // use .find() to feed in an array
+    // record.find(item => item.result) // this results in undefined
+    
+    const winningRecord = record.find(superbowlJudge); // I'm sure .find finds the FIRST OBJECT so why is it always returning undefined?????
+    // console.log(winningRecord.year);
+    if (winningRecord) return winningRecord.year;
+    else return winningRecord;
+}
+
+function superbowlJudge(record){
+    // console.log(year);
+
+    if(record.result===`W`){
+        return record;
+        // return String(year.year);
+    }    
+}
